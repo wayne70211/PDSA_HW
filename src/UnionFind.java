657ba1a4889d9dea589cb1f16bb22f508e153342
Wei <wayne70211@gmail.com>
2019-09-17 18:10:42 +0800

    Add WeightedQuickUnionUF with Dynamic Array

--- src/UnionFind.java
+++ src/UnionFind.java
@@ -1,54 +1,99 @@
-import java.util.Enumeration;
+import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Scanner;
 
 
 public class UnionFind {
+    private int count;      // number of components
+    private ArrayList<Integer> parent ;
+    private ArrayList<Integer> size   ;
+
+    private int count() {
+        return count;
+    }
+
+    private int find(int p) {
+        while (p != parent.get(p))
+            p = parent.get(p);
+        return p;
+    }
+
+    private boolean connected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    private void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+
+        // make smaller root point to larger one
+        if (size.get(rootP) < size.get(rootQ)) {
+            parent.set(rootP, rootQ);
+            size.set(rootQ, size.get(rootP));
+        }
+        else {
+            parent.set(rootQ, rootP);
+            size.set(rootP, size.get(rootQ));
+        }
+        count--;
+    }
+
     public static void main(String[] args) {
 
         Hashtable<String, Integer> table = new Hashtable<>();
+        UnionFind uf = new UnionFind();
+
+        uf.parent = new ArrayList<>();
+        uf.size = new ArrayList<>();
 
         Scanner in = new Scanner(System.in);
 
         // Read threshold
         int threshold = Integer.parseInt(in.nextLine());
-        int count = 0;
 
-        while (!in.hasNext("Q")){
+        // Initializes index
+        int index = 0;
+
+        while (in.hasNext()){
             // Read Line
             String line = in.nextLine();
 
-            // Split the Content
-            String[] protein = line.split("\t");
+            try {
+                // Split the Content
+                String[] protein = line.split("\t");
 
-            // Get the strength
-            int strength = Integer.parseInt(protein[2]);
+                // Get the strength
+                int strength = Integer.parseInt(protein[2]);
 
-            if (strength >= threshold){
+                if (strength >= threshold){
 
-                // Write the Hashtable
-                if (!table.containsKey(protein[0])) {
-                    table.put(protein[0], count);
-                    count++;
+                    // Write the Hashtable
+                    for (int i=0;i<2;i++){
+                        if (!table.containsKey(protein[i])) {
+                            table.put(protein[i], index);
+
+                            // Initializes WeightedQuickUnionUF
+                            uf.parent.add(index);
+                            uf.size.add(1);
+                            uf.count +=1;
+
+                            index++;
+                        }
+                    }
+
+                    // Weighted Quick-Union
+                    if (uf.connected(table.get(protein[0]), table.get(protein[1]))) continue;
+                    uf.union(table.get(protein[0]),table.get(protein[1]));
+
                 }
-                if (!table.containsKey(protein[1])) {
-                    table.put(protein[1], count);
-                    count++;
-                }
-
-                // Weighted Quick-Union
-
+            }catch (Exception e){
+                break;
             }
         }
 
-        Enumeration<String> names = table.keys();
-
-        while (names.hasMoreElements()){
-            String name = names.nextElement();
-            System.out.println( name + " : " +table.get(name));
-        }
-        System.out.println(count);
-
+        System.out.println(index);
+        System.out.println(uf.count());
 
     }
 }
